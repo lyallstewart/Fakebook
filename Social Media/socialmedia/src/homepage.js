e92e5ff57c51010af71161417b41993b828069f7
48panda48 <george.48panda@gmail.com>
2021-07-14 19:09:02 +0100

    Split homepage into three columns

--- Social Media/socialmedia/src/homepage.js
+++ Social Media/socialmedia/src/homepage.js
@@ -4,15 +4,22 @@ import './index.css'
 class HomePage extends React.Component {
   render() {
     return (
-      <div>
+      <>
         <nav>
           <ul>
-            <li><img id="logo" src="https://i.ytimg.com/vi/oHg5SJYRHA0/hqdefault.jpg" /></li>
+            <li><img id="logo" src="https://i.ytimg.com/vi/oHg5SJYRHA0/hqdefault.jpg" alt="A profile"/></li>
             
             <li><button id="MenuBar">Menu</button></li>
           </ul>
         </nav>
-      </div>
+        <div className="flex-container">
+        <div className="flex-item verticalItem" id="friends"></div>
+        <div className="flex-item verticalSpacer"></div>
+        <div className="flex-item verticalItem" id="posts"></div>
+        <div className="flex-item verticalSpacer"></div>
+        <div className="flex-item verticalItem" id="SuggestedFriends"></div>
+        </div>
+      </>
     )
   }
 }
