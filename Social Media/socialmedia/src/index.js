34ef48e382d3cb471974eb55d845ee167de2a79f
Lyall Stewart <lyallstewart@outlook.com>
2021-07-14 07:40:20 +0100

    Updated navbar formatting

--- Social Media/socialmedia/src/index.js
+++ Social Media/socialmedia/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import HomePage from './App';
+import HomePage from './homepage';
 
 ReactDOM.render(
   <React.StrictMode>
