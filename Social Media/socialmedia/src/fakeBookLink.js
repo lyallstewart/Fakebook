cebabc090335e7d70452b9575a457e4fbe454d99
48panda48 <george.48panda@gmail.com>
2021-07-20 08:38:42 +0100

    Added link class for convenience

---
+++ Social Media/socialmedia/src/fakeBookLink.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import history from './history'
+
+export class FakeBookLink extends Component {
+    constructor() {
+        super()
+        this.linkClicked = this.linkClicked.bind(this)
+    }
+    linkClicked(e) {
+        e.preventDefault()
+        history.push(this.props.href)
+    }
+    render() {
+        return (
+            <a onClick={this.linkClicked} href={this.props.href} className={this.props.className} style={this.props.style}>
+                {this.props.children}
+            </a>
+        )
+    }
+}
+
+export default FakeBookLink
