0f6770c90a5f210465d249607482dbc7682b00af
48panda48 <george.48panda@gmail.com>
2021-07-24 15:55:36 +0100

    encryption with big integers

--- EndToEnd/frontend/encryption.js
+++ EndToEnd/frontend/encryption.js
@@ -3,6 +3,13 @@ We need to encrypt messages and achieve end to end encryption.
 I am going to impliment RSA. Probably not 2048 bits though.
 It would be quite cool to fit the public key onto a QR code, and use that as the way to establish a connection
 */
+var forge = require('node-forge');
+//Apparently it is quite hard to find chonky primes, and my testing with small primes confirms this.
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 function isPrime(n,k) {
     /*We need to be able to check if something is prime. 
     This is the algorithm that I am following: https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test#Miller%E2%80%93Rabin_test
@@ -50,12 +57,12 @@ function gcd(a,b) {
 function extended_gcd(a,b) {
     let old_r = a
     let r = b
-    let old_s = 1
-    let s = 0
-    let old_t = 0
-    let t = 1
-    while(r!==0) {
-        let quotient = Math.floor(old_r/r);
+    let old_s = 1n
+    let s = 0n
+    let old_t = 0n
+    let t = 1n
+    while(r!==0n) {
+        let quotient = old_r/r
         //console.log(r,s,t);
         let temp = r
         r = old_r - (quotient*r)
@@ -69,35 +76,93 @@ function extended_gcd(a,b) {
     }
     return old_s
 }
-
+function powmod(b,e,mod) {
+    if(mod===1n){return 0n}
+    let r=1n
+    b%=mod
+    while(e>0n){if(e%2n==1n){r=(r*b)%mod}e=e/2n;b=(b*b)%mod}
+    return r
+}
+   
 class RSAEncryption {
     constructor() {
-
+        this.type=""
+        this.generateKeySet = this.generateKeySet.bind(this)
+        this.fromPrivateKey = this.fromPrivateKey.bind(this)
+        this.fromPublicKey = this.fromPublicKey.bind(this)
+        this.encrypt = this.encrypt.bind(this)
+        this.decrypt = this.decrypt.bind(this)
     }
-    generateKeySet=()=>{
-        let p = 0
-        while (true) {
-            if (isPrime(p,100)) {break}
-            p=Math.floor(Math.random()*100)
-        }
-        let q = 0
-        while (true) {
-            if (isPrime(q,100)) {break}
-            q=Math.floor(Math.random()*100)
-        }
-        let n = p*q
-        let lambda_n = ((p-1)*(q-1))
-        console.log(lambda_n)
-        let e=0
-        while (true) {
-            if (gcd(e,lambda_n)===1) {break}
-            e=Math.floor(Math.random()*(lambda_n-2))
-            console.log(e)
-        }
-        let d=extended_gcd(e,lambda_n)
-        console.log(d,e,n)
+    async generateKeySet(){
+        if(this.type){throw "Already declared encryption"}
+        var self = this;
+        let promise = new Promise(function(myResolve, myReject) {
+          forge.prime.generateProbablePrime(1024, (err, p_obj) => {
+            if(err){throw err}
+              forge.prime.generateProbablePrime(1024, (err, q_obj) => {
+                if(err){throw err}
+                let p = BigInt(p_obj.toString(10))
+                let q = BigInt(q_obj.toString(10))
+                console.log("FOUND PRIMES")
+                let n = p*q
+                let lambda_n = ((p-1n)*(q-1n))/gcd(p-1n,q-1n)
+                let e=65537n
+                while (true) {
+                    if (gcd(e,lambda_n)===1n) {break}
+                    e++
+                }
+                console.log("FOUND E")
+                let d=extended_gcd(e,lambda_n)
+                d = d % lambda_n;
+                if(d < 0)
+                    d += lambda_n;
+                self.d = d
+                self.e = e
+                self.n = n
+                self.public = {e,n}
+                self.private = {d,e,n}
+                self.type = "private"
+                myResolve()
+              });
+          })
+          
+        });
+        await promise
+
+          
+        
+    }
+    fromPublicKey(pub){
+        if(this.type){throw "Already declared encryption"}
+        this.public = pub
+        this.e = pub.e
+        this.n = pub.n
+        this.type = "public"
+    }
+    fromPrivateKey(priv){
+        if(this.type){throw "Already declared encryption"}
+        this.private = priv
+        this.public = {e:priv.e,n:priv.n}
+        this.d = priv.d
+        this.e = priv.e
+        this.n = priv.n
+        this.type = "private"
+    }
+    encrypt(m){
+        return powmod(m,this.e,this.n)
+    }
+    decrypt(m){
+        return powmod(m,this.d,this.n)
     }
 }
-
-a = new RSAEncryption
-a.generateKeySet()
+main = async ()=>{
+    a = new RSAEncryption()
+    await a.generateKeySet()
+    console.log(a.private)
+    start = new Date().getTime()
+    b=a.encrypt(3524627427n)
+    c=a.decrypt(b)
+    end=new Date().getTime()
+    console.log(`TIME:${end-start}ms`)
+}
+main()
