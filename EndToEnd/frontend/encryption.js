76b58d55df08ac299684f5691e19d63ebb7b3a66
48panda48 <george.48panda@gmail.com>
2021-07-25 18:16:11 +0100

    Added helper functions

--- EndToEnd/frontend/encryption.js
+++ EndToEnd/frontend/encryption.js
@@ -1,8 +1,11 @@
 /*
 We need to encrypt messages and achieve end to end encryption.
-I am going to impliment RSA. Probably not 2048 bits though.
-It would be quite cool to fit the public key onto a QR code, and use that as the way to establish a connection
+I am going to impliment RSA. Probably not 2048 bits though. Yeah let's go 512 bits. still secure, i think...
 */
+var CryptoJS = require("crypto-js")
+
+//Get ready for compressed code! It is just because I need this in quite a few places.
+function SHA256(string){return CryptoJS.enc.Base64.stringify(CryptoJS.SHA256(string))}function B64ToStr(base64){return CryptoJS.enc.Utf8.stringify(CryptoJS.enc.Base64.parse(base64))}function StrToB64(string){return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Utf8.parse(string))}function NumToB64(number){return CryptoJS.enc.Base64.stringify(CryptoJS.enc.Hex.parse(number.toString(16)))}function B64ToNum(base64){return parseInt("0x00"+CryptoJS.enc.Hex.stringify(CryptoJS.enc.Base64.parse(base64)))}function B64ToBigInt(base64){return BigInt("0x00"+CryptoJS.enc.Hex.stringify(CryptoJS.enc.Base64.parse(base64)))}
 var forge = require('node-forge');
 //Apparently it is quite hard to find chonky primes, and my testing with small primes confirms this.
 function getRandomInt(min, max) {
@@ -83,7 +86,7 @@ function powmod(b,e,mod) {
     while(e>0n){if(e%2n==1n){r=(r*b)%mod}e=e/2n;b=(b*b)%mod}
     return r
 }
-   
+ 
 class RSAEncryption {
     constructor() {
         this.type=""
@@ -97,9 +100,9 @@ class RSAEncryption {
         if(this.type){throw "Already declared encryption"}
         var self = this;
         let promise = new Promise(function(myResolve, myReject) {
-          forge.prime.generateProbablePrime(1024, (err, p_obj) => {
+          forge.prime.generateProbablePrime(256, (err, p_obj) => {
             if(err){throw err}
-              forge.prime.generateProbablePrime(1024, (err, q_obj) => {
+              forge.prime.generateProbablePrime(256, (err, q_obj) => {
                 if(err){throw err}
                 let p = BigInt(p_obj.toString(10))
                 let q = BigInt(q_obj.toString(10))
@@ -154,6 +157,9 @@ class RSAEncryption {
     decrypt(m){
         return powmod(m,this.d,this.n)
     }
+    bigIntToBase64(n){
+        return n.toString(8).match(/..?(?=(?:..)*$)/g).map(x=>"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/"[parseInt(x,8)]).join('')
+    }
 }
 main = async ()=>{
     a = new RSAEncryption()
@@ -162,6 +168,8 @@ main = async ()=>{
     start = new Date().getTime()
     b=a.encrypt(3524627427n)
     c=a.decrypt(b)
+    console.log(a.p)
+    console.log(a.bigIntToBase64(a.n))
     end=new Date().getTime()
     console.log(`TIME:${end-start}ms`)
 }
